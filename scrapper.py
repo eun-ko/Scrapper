1f4b97ddbcb028fcb3742c63047561dd64a24826
EunKo <koeun0529@gmail.com>
2020-06-01 19:23:21 +0900

    change get_last_page

--- scrapper.py
+++ scrapper.py
@@ -3,37 +3,32 @@ import urllib.request
 from bs4 import BeautifulSoup
 import csv
 
-db={}
-URL="https://store.musinsa.com/app/items/lists/015/?category=&d_cat_cd=015&u_cat_cd=&brand=&sort=pop&sub_sort=&display_cnt=90"
+URL="https://store.musinsa.com/app/items/lists/001/?category=&d_cat_cd=001&u_cat_cd=&brand=&sort=pop&sub_sort=&display_cnt=90"
 
 def get_last_page():
     result=requests.get(URL)
     soup=BeautifulSoup(result.text,"html.parser")
-    paging=soup.find("div",{"class":"pagination"}).find("div",{"class":"wrapper"}).find_all("a")
-    pages=[]
-    for page in paging:
-        if page.get_text(strip=True):
-            pages.append(int(page.get_text(strip=True)))
-
-    max_page=pages[-1]
-    return int(max_page)
-
+    last_page=soup.find("span",{"class":"pagingNumber"}).find("span",{"class":"totalPagingNum"}).get_text().strip()
+    print(f"이 카테고리는 {last_page}페이지 까지 있습니당")
+    return int(last_page)
 
 def get_image(max_page):
     for page in range(max_page):
         result=requests.get(f"{URL}&page={page+1}")
         soup=BeautifulSoup(result.text,"html.parser")
         img=soup.select("#searchList > li > div.li_inner > div.list_img > a > img")
-        print(f"scrapping page {page+1}\n")
+        print(f"{page+1}페이지 긁어오는중~~~\n")
         for i in img:
             imgUrl=i["data-original"]
             print("https:"+imgUrl)
-            urllib.request.urlretrieve("https:"+imgUrl,i["alt"]+'.jpg')
+            urllib.request.urlretrieve("https:"+imgUrl,(i["alt"]+'.jpg').replace("/"," "))
+            #이미지 저장시 상품명에 '/'포함시 디렉토리 경로로 인식함ㅠ->/를 space로 대체
 #urlretrieve는 다운로드 함수
 #img.alt는 이미지 대체 텍스트
 
+
 def save_to_file(infos):
-    file=open("infos.csv",mode="w")
+    file=open("infos.csv",mode="w",encoding='utf-8',newline='')
     writer=csv.writer(file)
     #open한 파일에 작성할거임
     writer.writerow(["brand-name","item-name","price"])
@@ -45,7 +40,7 @@ def save_to_file(infos):
 def get_items(max_page):
     list=[]
     for page in range(max_page):
-        #페이지 별로 긁어오기(0~9인데 +1한 값으로 이동되게)
+        #페이지 별로 긁어오기(for~range니까 +1한 값으로 이동되게)
         result=requests.get(f"{URL}&page={page+1}")
         soup=BeautifulSoup(result.text,"html.parser")
         brandName=soup.select("#searchList > li > div.li_inner > div.article_info > p.item_title")
@@ -62,10 +57,10 @@ def get_items(max_page):
                 #그 태그를 없애기..price[i]안에서 없어지는건가?
             infos={"brand-name":brandName[i].get_text(strip=True),"item-name":itemName[i].get_text(strip=True),"price":price[i].get_text(strip=True)}
             list.append(infos)
-        print(f"scrapping page {page+1}\n")
+        print(f"{page+1}페이지 긁어오는중~~~\n")
     save_to_file(list)
     return list
-        
 
-get_items(get_last_page())
-    
+save_to_file(get_items(get_last_page()))        
+get_image(get_last_page())
+
