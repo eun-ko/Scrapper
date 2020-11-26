dc1fbe68483c214c7399cf8b01e1cf0fa17eac9d
EunKo <koeun0529@gmail.com>
2020-11-27 06:05:17 +0900

    [feat] 스크롤 기능 추가

--- responsive_automation.py
+++ responsive_automation.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from config import CHROME_DRIVER_PATH,RESPONSIVE_URL
 
 import time
+import math
+
+MY_BROWSER_HEIGHT=1296
 
 driver=webdriver.Chrome(
   executable_path=CHROME_DRIVER_PATH
@@ -9,11 +12,22 @@ driver=webdriver.Chrome(
 driver.get(RESPONSIVE_URL)
 driver.maximize_window() #브라우저 크기 최대치로
 
-browser_width=[320,480,960,1366,1920] #height은 스크롤할거니까 상관X
+browser_width=[480,960,1366,1920] #height은 스크롤할거니까 상관X
 
 print(driver.get_window_size()) 
 #height확인용
 
 for width in browser_width:
-  driver.set_window_size(browser_width,1296)
-  time.sleep(5)
+  driver.set_window_size(width,MY_BROWSER_HEIGHT)
+  time.sleep(3)
+  driver.execute_script("window.scrollTo(0,0)") #맨 위로 이동
+  scroll_size=driver.execute_script("return document.body.scrollHeight") 
+  #return-파이썬으로 반환됨
+  #print(scroll_size/MY_BROWSER_HEIGHT)
+  #scroll size/ browser size ==스크롤 횟수
+  total_scroll_numbers=math.ceil(scroll_size / MY_BROWSER_HEIGHT)
+  
+  for scroll_number in range(total_scroll_numbers):
+    driver.execute_script(f"window.scrollTo(0,{(scroll_number+1)*MY_BROWSER_HEIGHT})")
+    time.sleep(2)
+  
