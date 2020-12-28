c69835f6e88dcffc56d633e37f2e782a91553560
EunKo <koeun0529@gmail.com>
2020-12-29 01:26:53 +0900

    [feat] pagination 추가

--- automation_tester/searchbot.py
+++ automation_tester/searchbot.py
@@ -1,3 +1,7 @@
+#import os,sys
+#sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from config import CHROME_DRIVER_PATH,RESPONSIVE_URL
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -5,8 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 #로드 느린것들 대기
-
-from config import CHROME_DRIVER_PATH,RESPONSIVE_URL
 
 KEYWORD=""
 
@@ -23,16 +25,21 @@ search_bar.send_keys(Keys.ENTER)
 #send_keys는 키보드 인풋만 가능. 변수 전달 x
 
 elements_to_exclude=WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CLASS_NAME,"g-blk")))
-#element 위치 생길때까지 wait , 필요없는 요소 삭제
+#element 위치 생길때까지 wait , 필요없는 요소(첫페이지에만 등장) 삭제
 
-driver.execute_script("""
-const exclude=arguments[0];
-exclude.parentElement.removeChild(exclude)""",elements_to_exclude)
+pagination=driver.find_elements_by_tag_name("td")
+del pagination[0:2]
 
-search_results_wrapper=driver.find_elements_by_class_name("g")
+flag=True
+#첫페인지 판별
+for pagination_index,p in enumerate(pagination):
+  if flag:
+    driver.execute_script("""
+    const exclude=arguments[0];
+    exclude.parentElement.removeChild(exclude);""",elements_to_exclude)
+    flag=False
+  search_results_wrapper=driver.find_elements_by_class_name("g")
+  for index,search_result in enumerate(search_results_wrapper):
+    search_result.screenshot(f"screenshots/{KEYWORD}_{pagination_index+1}_{index+1}.png")
+  p.click()
 
-for index,search_result in enumerate(search_results_wrapper):
-  search_result.screenshot(f"screenshots/{KEYWORD}_{index+1}.png")
-  #class_attribute=search_result.get_attribute("class")
-  #if "kno-kp mnr-c g-blk" not in class_attribute:
-  #  search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
